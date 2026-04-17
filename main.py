5293d613772ebf11a49503066592c0e56919ec37
Vaibhav Madaan <madaan.vaibhav88@gmail.com>
2026-04-17 11:39:05 +0530

    reworked on add button

--- main.py
+++ main.py
@@ -6,32 +6,20 @@ label = sg.Text('Enter a ToDo:')
 input1 = sg.InputText(tooltip='Enter ToDo', key='todo')
 add_button = sg.Button("Add")
 
-list_box = sg.Listbox(values=read_file(), key='todos',
-                      enable_events=True, size=[45, 10])
-edit_button = sg.Button('Edit')
-
-layout = [[label], [input1, add_button], [list_box, edit_button]]
+layout = [[label], [input1, add_button]]
 
 window = sg.Window('To-Do App', layout, font=('Helvetica', 16))
+
 while True:
     event, values = window.read()
-    print(event)
-    print(values)
     match event:
         case 'Add':
             todos = read_file()
-            new_todo = values['todo'] + '\n'
+            new_todo = values['todo']
             todos.append(new_todo)
             write_to_file(todos)
-        case 'Edit':
-            todo_to_edit = values['todos'][0]
-            new_todo = values['todo']
-            todos = read_file()
-            index_of_todo_to_edit = todos.index(todo_to_edit)
-            todos[index_of_todo_to_edit] = new_todo + '\n'
-            write_to_file(todos)
-            window['todos'].update(values=todos)
         case sg.WIN_CLOSED:
             break
-
+    print(event)
+    print(values)
 window.close()
